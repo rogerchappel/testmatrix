f235852729d4065bfa603915ce1a73ad3535d500
Roger Chappel <miscanalysis@gmail.com>
2026-05-18 06:34:18 +1000

    chore: expose public library entrypoints

---
+++ src/index.ts
@@ -0,0 +1,5 @@
+export { classifyCommand } from './classify.js';
+export { detectCommands } from './detect.js';
+export { runCommands } from './runner.js';
+export { createMatrix } from './matrix.js';
+export type { CandidateCommand, CliOptions, CommandKind, ResultMatrix, RunResult } from './types.js';
